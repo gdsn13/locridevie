bc113dab91fa1e55781bd5de0d5ea286130b9753
gumar <gumar@MacBook-Pro-de-gumar.local>
2012-05-24 17:48:26 +0200

    couluer pour editeur

--- public/javascripts/admin/application.js
+++ public/javascripts/admin/application.js
@@ -59,7 +59,7 @@ var TinyMceDefaultSettings = {
   width: '709',
   inlinepopups_skin: 'locomotive',
   convert_urls: false,
-	theme_advanced_text_colors: "FDC400,1285bc,cc071e,009D85",
+	theme_advanced_text_colors: "000000,FFFFFF,FDC400,1285bc,cc071e,009D85",
   fullscreen_new_window : false,
   fullscreen_settings : {
     theme_advanced_path_location : "top"
