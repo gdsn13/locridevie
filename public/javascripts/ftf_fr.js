8edc33e3206f9986e61ebf41f661736a1d48e841
gumar <gumar@MacBook-Pro-de-gumar.local>
2012-05-30 15:46:05 +0200

    pack lang ftf

---
+++ public/javascripts/ftf_fr.js
@@ -0,0 +1,17 @@
+{
+    "text_download" : "Télécharger",
+    "text_upgrade_now" : "Mettre à niveau",
+    "text_you" : "VOUS",
+    "button_continue" : "Continuer",
+    "button_close" : "Fermer",
+    "text_you_are_using" : "Vous utilisez",
+    "message_failed" : "Il est fortement recommander d'utiliser un autre navigateur.",
+    "message_acceptable" : "Pas mal. Essayez d'utiliser nos autres recommandations.",
+    "message_recommended" : "Bon choix, n'en changez pas.",
+    "message_error" : "Désolé, quelque chose s'est mal passé. Nous cherchons à le corriger.",
+    "status_loading" : "Chargement...",
+    "status_approved" : "Navigateur conforme.",
+    "status_acceptable" : "Navigateur accepté.",
+    "status_failed" : "Navigateur obsolète.",
+    "link_check" : "Tester"
+}
