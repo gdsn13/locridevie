d8c6e2d0abfdfdad847d4d07531f7de4487998ca
gumar <gumar@MacBook-Pro-de-gumar.local>
2013-06-04 12:13:35 +0200

    with cache

--- app/controllers/front/datas_controller.rb
+++ app/controllers/front/datas_controller.rb
@@ -1,7 +1,7 @@
 class Front::DatasController < ApplicationController
 
   respond_to :json
-  #caches_action :spectacle_list, :get_page, :get_dates, :get_intro, :get_spectacle
+  caches_action :spectacle_list, :get_page, :get_dates, :get_intro, :get_spectacle
   #caches_page :spectacle_list, :get_page, :get_dates, :get_intro, :get_spectacle, :newsletters
   
   def spectacle_list
