d66ac291798a7ef9992dd7db7043c9dbb643855c
gumar <gumar@new-host-4.home>
2013-06-11 16:37:04 +0200

    removing cache on actus

--- app/controllers/admin/actus_controller.rb
+++ app/controllers/admin/actus_controller.rb
@@ -3,8 +3,15 @@ module Admin
 
     sections 'contents', 'news'
     
+    before_filter :remove_cache
+    
     def index
       @actus = Actu.for_season(@current_site.season_back)
-    end    
+    end
+    
+    def remove_cache
+      expire_action :controller => '/front/datas', :action => 'get_intro'
+    end
+    
   end
 end
